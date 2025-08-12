d549d58564cb1bd81838e8fb0653cd2ddaf4f35a
ZyleStacion <s4064846@rmit.edu.vn>
2025-08-12 12:17:10 +0000

    Add blockchain class

--- app.py
+++ app.py
@@ -9,10 +9,21 @@ import random
 
 # Block Structure
 class Block:
-    def __init__(self, id, timestamp, data, previous_hash, nonce, hash):
+    """
+    Represent a single block in our chain.
+
+    Attributes:
+        id (int): Unique block ID.
+        timestamp (str): Time of block creation.
+        transactions (dict): A list of transactions containing transaction details.
+        previous_hash (str): SHA-256 hash of the previous block in the chain.
+        nonce (int): Arbitrary number used to track Proof-of-Work difficulty.
+        hash (str): SHA-256 hash of the block contents.
+    """
+    def __init__(self, id, timestamp, transactions, previous_hash, nonce, hash):
         self.id = id
         self.timestamp = timestamp
-        self.data = data
+        self.transactions = transactions
         self.previous_hash = previous_hash
         self.nonce = nonce
         self.hash = hash
@@ -32,3 +43,48 @@ class Block:
         bottom = "└" + "─" * (width - 2) + "┘"
         return "\n".join([top] + lines + [bottom])
     
+# Chain integrity and hashing
+# Source: https://hackernoon.com/learn-blockchains-by-building-one-117428612f46
+class Blockchain(object):
+    """
+    Manages the chain by storing transactions and contains helper methods to add new blocks.
+    """
+    def __init___(self):
+        self.chain = []
+        self.current_transactions = []
+    
+    def new_block(self):
+        # Creates a new block and adds it to the chain
+        pass
+
+    def new_transaction(self, sender, recipient, amount):
+        """
+        Creates a new transaction which will be placed into the next mined Block
+
+        Args:
+
+        """
+        pass
+
+    @staticmethod
+    def hash(block):
+        """
+        Creates a SHA-256 hash for a block. @staticmethod = can be used outside of the 'Blockchain' class.
+
+        Args:
+            block (dict): Block
+        
+        Returns: 
+            hash (str): Hash value
+        """
+        pass
+
+    @property
+    def last_block(self):
+        """
+        Returns the last block in the chain. @property = turns a method into an attribute so we can modify its values later on.
+
+        Returns:
+            block (dict): Last block
+        """
+    pass
